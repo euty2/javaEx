3af70f2e0296eb33eb6bebebba8e9ee21a70ccc7
SJF05 <lakors05@gmail.com>
2025-05-13 08:52:10 +0900

    Problems01

---
+++ _04_Resolve/src/$01.java
@@ -0,0 +1,10 @@
+public class $01 {
+    public static void main(String[] args) {
+        // 1. while문을 이용해서 1부터 100까지 출력하세요
+        int i = 1;
+        while(i <= 100){
+            System.out.println(i);
+            i++;            
+        }
+    }
+}
