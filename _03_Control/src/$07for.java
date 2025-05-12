1d7d1d4b427c71786ef9426e546255741645767b
SJF05 <lakors05@gmail.com>
2025-05-12 16:27:13 +0900

    제어문 실습 완료

--- _03_Control/src/$07for.java
+++ _03_Control/src/$07for.java
@@ -1,12 +1,10 @@
-public class $05while {
+public class $07for {
     public static void main(String[] args) {
         int num = 1;        // 초기식
 
-        // 조건식
-        while(num <= 100){
+        for(;num <= 10;){
             System.out.println("나무를 " + num + "번 두드렸습니다.");
 
-            // 증감식
             num++;
         }
         System.out.println("나무를 캐었다~~~");
