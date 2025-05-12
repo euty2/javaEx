dcfa16f7edcfa5913779d1cb07507560d607fc5b
SJF05 <lakors05@gmail.com>
2025-05-12 15:56:07 +0900

    while문 실습

---
+++ _03_Control/src/$05while.java
@@ -0,0 +1,10 @@
+public class $05while {
+    public static void main(String[] args) {
+        int num = 1;
+        while(num <= 100){
+            System.out.println("나무를 " + num + "번 두드렸습니다.");
+            num++;
+        }
+        System.out.println("나무를 캐었다~~~");
+    }
+}
