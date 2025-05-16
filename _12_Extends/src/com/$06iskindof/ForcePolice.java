cf7078674913eeb853aebaff91c01f8e6330b095
SJF05 <lakors05@gmail.com>
2025-05-16 12:48:07 +0900

    상속속

---
+++ _12_Extends/src/com/$06iskindof/ForcePolice.java
@@ -0,0 +1,15 @@
+package com.$06iskindof;
+
+public class ForcePolice extends Police {
+    public ForcePolice(int bullet, int handCuffs){
+        super(bullet, handCuffs);
+    }
+
+    public void run(){
+        System.out.println("Running!");
+    }
+
+    public void fight(){
+        System.out.println("Fight!");
+    }
+}
